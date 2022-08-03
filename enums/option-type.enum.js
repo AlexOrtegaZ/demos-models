6f68f244984706d0ec337110db171e7b1ba71cb3
YaelBadillo <yael.badillos@gmail.com>
2022-08-02 19:34:22 -0500

    Feat: add another option type.
    This new option type is SEGGESTION_TO_PROPOSAL = 2.

--- enums/option-type.enum.js
+++ enums/option-type.enum.js
@@ -18,9 +18,10 @@
 */
 
 const optionTypeEnum = {
-    IN_FAVOR_OR_OPPOSSING: 0,
-    MULTIPLE_OPTIONS: 1 
-  };
-  
-  module.exports = optionTypeEnum;
+  IN_FAVOR_OR_OPPOSSING: 0,
+  MULTIPLE_OPTIONS: 1,
+  SUGGESTION_TO_PROPOSAL: 2
+};
+
+module.exports = optionTypeEnum;
   
