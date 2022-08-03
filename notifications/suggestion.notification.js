9b0b472fe4097e6a0b33d6a3a1fb8214eef9fc18
YaelBadillo <yael.badillos@gmail.com>
2022-08-03 02:35:31 -0500

    Feat: add createSuggestionsCache.

---
+++ notifications/suggestion.notification.js
@@ -0,0 +1,38 @@
+/*
+  DEMOS
+  Copyright (C) 2022 Julian Alejandro Ortega Zepeda, Erik Ivanov Domínguez Rivera, Luis Ángel Meza Acosta
+  This file is part of DEMOS.
+
+  DEMOS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DEMOS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+const cacheService = require('../services/cache.service');
+const CacheRepository = require('../repositories/cache.repository');
+const MemberRepository = require('../repositories/member.repository');
+const { PUBLISHED, UPDATED } = require('../constants/event-names');
+const { PROPOSALS } = require('../constants/entity-names');
+
+
+const createSuggestionsCache = (eventName, userId, data) => {
+  return CacheRepository.createCache(PROPOSALS, eventName, userId, data);
+};
+
+
+const suggestionUpdated = () => {
+};
+
+module.exports = {
+  createSuggestionsCache,
+  suggestionUpdated
+};
